04e76a7ce652fb9a1bd593fcaba9e8cef8db1b76
Katie Sharples <KSharples431@gmail.com>
2023-02-22 16:42:50 -0500

    models.js added

---
+++ models.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+let bookSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Series: { type: String, required: true },
+  SeriesNumber: { type: Number, required: true },
+  Genre: {
+    Name: String,
+    Description: String,
+  },
+  Author: {
+    Name: String,
+    Bio: String,
+  },
+  Actors: [String],
+  ImagePath: String,
+  haveRead: Boolean,
+  owned: Boolean,
+});
+
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  Favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
+});
+
+let Book = mongoose.model('Book', bookSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Book = Book;
+module.exports.User = User;
