49ba4259d127fec06909868f7fd5dc1ce60124a8
Katie Sharples <KSharples431@gmail.com>
2023-03-08 15:03:36 -0500

    Added cors

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ app.use(
   })
 );
 
+// Cors middleware
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          `The CORS policy for this application doesn’t allow access from origin ` +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 // Auth
 let auth = require('./auth')(app);
 const passport = require('passport');
