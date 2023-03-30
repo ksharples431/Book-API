5f70941392a308b0cc299cc4940208fdb3669d4a
Katie Sharples <KSharples431@gmail.com>
2023-03-30 15:20:32 -0400

    Revert

--- models.js
+++ models.js
@@ -27,7 +27,7 @@ userSchema.statics.hashPassword = (password) => {
 };
 
 userSchema.methods.validatePassword = function (password) {
-  return bcrypt.compareSync(password, this.Password);
+  return bcrypt.compareSync(password, this.password);
 };
 
 let Book = mongoose.model('Book', bookSchema);
